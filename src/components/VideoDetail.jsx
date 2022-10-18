b2491782563cb35a74464971e67a9d766845cc00
Timothy Austin <timothyonyea@gmail.com>
2022-10-18 17:08:44 +0100

    fixed video detail component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -9,24 +9,25 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
   const [videos, setVideos] = useState(null);
+  
   const { id } = useParams();
 
+  
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideoDetail(data.items[0]))
 
     fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then((data) => setVideos(data.items))
 
   }, [id]);
-console.log(videoDetail)
+  if(!videoDetail?.snippet) return 'Loading';
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount }} = videoDetail;  
+ 
 
-  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount }} = videoDetail;
-
-  return (
-    // <div>hey</div>
+  return (    
     <Box minHeight='95vh'>
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
-          <Box sx={{ width: "100", position: "sticky", top: "86px" }}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
             <ReactPlayer
               url={`https://www.youtube.com/watch?v=${id}`}
               className='react-player'
